03c4a6b9662329507c9741b48888d28049c88756
Khanbaba Fatullayev <fatullayev@usf.edu>
2025-12-17 08:02:11 +0400

    feat: configure Firebase environment variables and update emulator settings

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,18 +6,21 @@ import {getAuth, connectAuthEmulator} from "firebase/auth";
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAS1XGNj2IALs1Gu6nfwbwG5EzVDd9-tas",
-  authDomain: "truthguard-415e6.firebaseapp.com",
-  projectId: "truthguard-415e6",
-  storageBucket: "truthguard-415e6.firebasestorage.app",
-  messagingSenderId: "11350332056",
-  appId: "1:11350332056:web:021e895fc217a1b60536ed",
-  measurementId: "G-YT2LMLNPPG"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 //
 // // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 
 // Connect to Auth Emulator in development environment
-connectAuthEmulator(auth, "http://127.0.0.1:9099");
+if (import.meta.env.DEV) {
+    connectAuthEmulator(auth, import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST, {disableWarnings: true});
+}
